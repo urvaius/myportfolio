35aba7a9da6b72e95279a8b8d997492e3a53019c
urvaius <urvaius@gmail.com>
2022-03-24 14:54:04 -0700

    51.39 into video

--- app.js
+++ app.js
@@ -13,5 +13,22 @@ function PageTransitions(){
 			this.className += ' active-btn'
 		})
 	}
+	//sections active class
+	allSections.addEventListener('click', (e) =>{
+		const id = e.target.dataset.id;
+		if(id){
+			//remove selected from the other buttons
+			sectBtns.forEach((btn) =>{
+				btn.classList.remove('active')
+			})
+			e.target.classList.add('active')
+			// hide other sections
+			sections.forEach((section) =>{
+				section.classList.remove('active')
+			})
+			const element = document.getElementById(id);
+			element.classList.add('active');
+		}
+	})
 }
 PageTransitions();
